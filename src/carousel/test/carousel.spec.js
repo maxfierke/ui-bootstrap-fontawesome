42ded23407c38d45fb6e52951ece4763fa7fce20
Max Fierke <max@maxfierke.com>
2015-04-02 16:28:39 -0500

    feat(carousel): Add Font-Awesomized carousel templates
    Signed-off-by: Max Fierke <max@maxfierke.com>

---
+++ src/carousel/test/carousel.spec.js
@@ -0,0 +1,49 @@
+describe('carousel', function() {
+  beforeEach(module('ui.bootstrap.carousel', function($compileProvider, $provide) {
+    angular.forEach(['ngSwipeLeft', 'ngSwipeRight'], makeMock);
+    function makeMock(name) {
+      $provide.value(name + 'Directive', []); //remove existing directive if it exists
+      $compileProvider.directive(name, function() {
+        return function(scope, element, attr) {
+          element.on(name, function() {
+            scope.$apply(attr[name]);
+          });
+        };
+      });
+    }
+  }));
+  beforeEach(module('template/carousel/carousel.html', 'template/carousel/slide.html'));
+
+  var $rootScope, $compile;
+  beforeEach(inject(function(_$rootScope_, _$compile_) {
+    $rootScope = _$rootScope_;
+    $compile = _$compile_;
+  }));
+
+  describe('basics', function() {
+    var elm, scope;
+    beforeEach(function() {
+      scope = $rootScope.$new();
+      scope.slides = [
+        {active:false,content:'one'},
+        {active:false,content:'two'},
+        {active:false,content:'three'}
+      ];
+      elm = $compile(
+        '<carousel interval="interval" no-transition="true" no-pause="nopause">' +
+          '<slide ng-repeat="slide in slides" active="slide.active">' +
+            '{{slide.content}}' +
+          '</slide>' +
+        '</carousel>'
+      )(scope);
+      scope.interval = 5000;
+      scope.nopause = undefined;
+      scope.$apply();
+    });
+
+    it('has Font-Awesome icon classes for navigation arrows', function () {
+      expect(elm.find('.left.carousel-control > .fa.fa-chevron-left').length).toBe(1);
+      expect(elm.find('.right.carousel-control > .fa.fa-chevron-right').length).toBe(1);
+    });
+  });
+});
